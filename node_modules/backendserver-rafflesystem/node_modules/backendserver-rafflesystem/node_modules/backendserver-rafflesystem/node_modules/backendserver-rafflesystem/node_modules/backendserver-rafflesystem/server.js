ed1a2c4ea49c4af35e44bd63775fcab484aa5dd5
jalani <145871113+FaithQAQA@users.noreply.github.com>
2025-03-12 23:45:09 -0400

    fixed issue

--- node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/server.js
+++ node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/server.js
@@ -1,21 +1,11 @@
-// app.js
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const authRoutes = require('./Routes/authRoutes');
-const raffleRoutes = require('./Routes/raffleRoutes');
+// server.js
+const app = require('./app');
+const dotenv = require('dotenv');
 
-const app = express();
+dotenv.config();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api', raffleRoutes);
-
-// Connect to MongoDB
-connectDB();
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
