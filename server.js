0f31756414e07c179203da4166937309898141a0
jalani <145871113+FaithQAQA@users.noreply.github.com>
2025-03-12 22:53:42 -0400

    Normalize line endings to LF

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-// server.js
-const app = require('./app');
-const dotenv = require('dotenv');
+// app.js
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./Routes/authRoutes');
+const raffleRoutes = require('./Routes/raffleRoutes');
 
-dotenv.config();
+const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api', raffleRoutes);
+
+// Connect to MongoDB
+connectDB();
+
+module.exports = app;
