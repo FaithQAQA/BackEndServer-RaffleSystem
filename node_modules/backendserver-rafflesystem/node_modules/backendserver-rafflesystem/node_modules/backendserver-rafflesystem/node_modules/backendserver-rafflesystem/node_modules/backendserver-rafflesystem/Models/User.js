6396d530be8016218949c45cbea600d09f7680e3
jalani <145871113+FaithQAQA@users.noreply.github.com>
2025-03-14 12:56:06 -0400

    test

--- node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/Models/User.js
+++ node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/node_modules/backendserver-rafflesystem/Models/User.js
@@ -27,11 +27,6 @@ userSchema.methods.comparePassword = async function (password) {
 };
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+
 
 module.exports = mongoose.model('User', userSchema);
