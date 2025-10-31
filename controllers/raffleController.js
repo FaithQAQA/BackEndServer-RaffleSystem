65f6331d87bb9337bc5e290e444a48e8ed2b8cf0
jalani <145871113+FaithQAQA@users.noreply.github.com>
2025-10-30 20:23:36 -0400

    test

--- controllers/raffleController.js
+++ controllers/raffleController.js
@@ -75,18 +75,10 @@ const squareAPI = new SquareDirectAPI(process.env.SQUARE_ACCESS_TOKEN, 'sandbox'
 
 // ======================= EMAIL TRANSPORTER =======================
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
-  tls: {
-    rejectUnauthorized: false,
+    pass: process.env.GOOGLE_APP_PASSWORD,
   },
 });
 
